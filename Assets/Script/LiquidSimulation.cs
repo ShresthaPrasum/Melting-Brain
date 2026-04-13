48c1e04ca6de77ce0f8cf5229ca2eaeb36db44c2
prasum shrestha <prasumshrestha8877@gmail.com>
2026-04-13 15:36:26 +0545

    Liquid Simulation Done laptop is getting cooked

--- Assets/Script/LiquidSimulation.cs
+++ Assets/Script/LiquidSimulation.cs
@@ -34,7 +34,99 @@ public class LiquidSimulation : MonoBehaviour
 			return;
 		}
 
-		
+		if(waterDropPrefap == null || waterSpawnPoint == null)
+		{
+			Debug.LogWarning("BRO ASSIGN WATER DROP PREFAP");
+			return;
+		}
+
+		spawnedDropCount = 0 ;
+		spawnRoutine = StartCoroutine(SpawnWaterRoutine());
 	}
 
+	public void StopSpawningWater()
+	{
+		if(spawnRoutine == null)
+		{
+			return;
+		}
+		StopCoroutine(spawnRoutine);
+		spawnRoutine = null;
+	}
+
+	public void ResetAllWaterDrops()
+	{
+		StopSpawningWater();
+		spawnedDropCount = 0 ;
+
+		if(spawnedWaterParent != null)
+		{
+			for(int i = spawnedWaterParent.childCount-1;i>=0;i--)
+			{
+				Destroy(spawnedWaterParent.GetChild(i).gameObject);
+			}
+			return;
+		}
+		if(string.IsNullOrWhiteSpace(waterTagName))
+		{
+			return;	
+		}
+		GameObject[] drops = GameObject.FindGameObjectsWithTag(waterTagName);
+
+		for(int i = 0; i< drops.Length;i++)
+		{
+			Destroy(drops[i]);
+		}
+	}
+
+	private IEnumerator SpawnWaterRoutine()
+	{
+		if(waitBeforeStarting>0f)
+		{
+			yield return new WaitForSeconds(waitBeforeStarting);
+		}
+
+		while(spawnedDropCount<totalDropsToSpawn)
+		{
+			SpawnOneDrop();
+			spawnedDropCount++;
+
+			if(secondsBetweenDrops>0f)
+			{
+				yield return new WaitForSeconds(secondsBetweenDrops);
+			}
+			else
+			{
+				yield return null;
+			}
+		}
+		spawnRoutine = null;
+		FinishedSpawningWater?.Invoke();
+	}
+
+	private void SpawnOneDrop()
+	{
+		Vector3 spawnPosition = waterSpawnPoint.position;
+
+		if(addRandomXOffset && maxRandomXOffset > 0f)
+		{
+			spawnPosition.x += UnityEngine.Random.Range(-maxRandomXOffset,maxRandomXOffset);
+		}
+
+		if(spawnedWaterParent !=null)
+		{
+			Instantiate(waterDropPrefap, spawnPosition, Quaternion.identity, spawnedWaterParent);
+
+		}
+
+		else
+		{
+			Instantiate(waterDropPrefap, spawnPosition, Quaternion.identity);
+		}
+	}
+
+	private void OnDisable()
+	{
+		StopSpawningWater();
+	}
  }
